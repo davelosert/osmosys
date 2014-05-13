ae965268967917155048b30bf5f49cb52aaf83a3
Charminbear <david.losert@gmx.de>
2014-05-13 10:41:21 +0200

    Minor renaming of routes.

--- server_small.js
+++ server_small.js
@@ -9,10 +9,10 @@ MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
 		COLLECTION = db.collection('objectStorage');});
 var app = express();
 app.use(bodyParser.json());
-app.post('/save', function (req, res, next) {
+app.post('/object', function (req, res, next) {
 		COLLECTION.insert(req.body, function (err, result) {
 				res.send(200);});});
-app.get('/all', function (req, res, next) {
+app.get('/objects', function (req, res, next) {
 	COLLECTION.find().toArray(function (err, result) {
 			res.json(result);});});
 http.createServer(app).listen(3000, function () {console.log('Express server listening on port 3000');});
